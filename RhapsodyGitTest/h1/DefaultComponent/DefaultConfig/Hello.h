4bf1af8a0ec75f5df027f7b83b090e3c6fec9255
kotzjanm <kotzjanm@linux-xqf9.site>
2016-11-08 17:34:46 +0100

    Test commit of hello world rhapsody files

---
+++ RhapsodyGitTest/h1/DefaultComponent/DefaultConfig/Hello.h
@@ -0,0 +1,34 @@
+/*********************************************************************
+	Rhapsody	: 8.1.1 
+	Login		: kotzjanm
+	Component	: DefaultComponent 
+	Configuration 	: DefaultConfig
+	Model Element	: Hello
+//!	Generated Date	: Tue, 18, Oct 2016  
+	File Path	: DefaultComponent/DefaultConfig/Hello.h
+*********************************************************************/
+
+#ifndef Hello_H
+#define Hello_H
+
+//## auto_generated
+#include <oxf/oxf.h>
+//## package Default
+
+//## class Hello
+class Hello {
+    ////    Constructors and destructors    ////
+    
+public :
+
+    //## operation Hello()
+    Hello();
+    
+    //## auto_generated
+    ~Hello();
+};
+
+#endif
+/*********************************************************************
+	File Path	: DefaultComponent/DefaultConfig/Hello.h
+*********************************************************************/
